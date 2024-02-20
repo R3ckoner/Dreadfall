79733ba1b851dfe686a73fb21c4fd4560f424e36
Ruairi Delahunt <ruairidelahunt@gmail.com>
2024-02-20 13:04:11 -0800

    PlayerInteractionUpdate
    --can pickup weapons with tags

--- Assets/Scripts/Player/PlayerInteraction.cs
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class PlayerInteraction : MonoBehaviour
 {
     public float pickupRange = 2f;
-    public string pickupTag = "weaponPickup"; // Tag for the pickup objects
-    public string weaponTag = "weapon"; // Tag for the weapon
+    public List<string> pickupTags = new List<string> { "weaponPickup1", "weaponPickup2" }; // Tags for the pickup objects
+    public List<string> weaponTags = new List<string> { "weapon1", "weapon2" }; // Tags for the weapons
 
     private Camera playerCamera;
 
@@ -32,12 +33,12 @@ public class PlayerInteraction : MonoBehaviour
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, pickupRange))
         {
             GameObject pickupObject = hit.collider.gameObject; // Get the pickup object
-            if (pickupObject.CompareTag(pickupTag)) // Check the tag
+            if (pickupTags.Contains(pickupObject.tag)) // Check if the tag is in the list
             {
                 // Enable the weapon which is a child of the camera
                 foreach (Transform child in playerCamera.transform)
                 {
-                    if (child.gameObject.CompareTag(weaponTag))
+                    if (weaponTags.Contains(child.gameObject.tag))
                     {
                         child.gameObject.SetActive(true);
                         break;
